66a4d749421d582f8bd22398dfd5c6b58be855f9
Nikhil Pathak <nikhilpathak926@gmail.com>
2017-12-27 19:30:47 +0530

    Can update variable values
    Added feature which is able to update the values of variable accordingly visiting the graph

--- static_dynamic_functions.py
+++ static_dynamic_functions.py
@@ -32,7 +32,6 @@ def get_error_line_number(current_edge , data):
 
 def divide_by_zero(graph , initial_statment , variable_list , data):
 
-	divide_flg = 0
 	divide_by_zero_list = []
 	statment_stack = []
 	statment_stack.append(initial_statment)
@@ -67,3 +66,84 @@ def divide_by_zero(graph , initial_statment , variable_list , data):
 					if flg != 1:
 						get_error_line_number(current_edge , data)
 						divide_by_zero_list.append(current_edge)
+
+
+def calculate_all_loop_iterations(graph , initial_statment , variable_list):
+		
+	statment_stack = []
+	statment_stack.append(initial_statment)
+	while 1:
+
+		if len(statment_stack) == 0:
+			break
+		current_edge = statment_stack[0]
+		del statment_stack[0]
+
+		lexer = pt.get_lexer(current_edge)
+		current_token = lexer.token()
+		previous_token = current_token
+		assign_flg = 0
+		rhs_value = 0
+		operator_flg = 0
+		operator_type = "NOTHING"
+		while 1:
+			if not current_token:
+				break
+
+			if assign_flg == 1:
+				if current_token.type != 'ID' and current_token.type != 'NUMBER':
+					operator_flg = 1
+					operator_type = current_token.type
+				else:
+					if operator_flg == 1:
+
+						if operator_type == 'DIVIDE':
+							if current_token.type == 'NUMBER': 
+								rhs_value = rhs_value / current_token.value
+							else:
+								rhs_value = rhs_value / variable_list[current_token.value]
+							variable_list[lhs_variable] = rhs_value
+
+						if operator_type == 'MULT':
+							if current_token.type == 'NUMBER': 
+								rhs_value = rhs_value * current_token.value
+							else:
+								rhs_value = rhs_value * variable_list[current_token.value]
+							variable_list[lhs_variable] = rhs_value
+								
+						if operator_type == 'PLUS':
+							if current_token.type == 'NUMBER': 
+								rhs_value = rhs_value + current_token.value
+							else:
+								rhs_value = rhs_value + variable_list[current_token.value]
+							variable_list[lhs_variable] = rhs_value
+
+						if operator_type == 'MINUS':
+							if current_token.type == 'NUMBER': 
+								rhs_value = rhs_value - current_token.value
+							else:
+								rhs_value = rhs_value - variable_list[current_token.value]
+							variable_list[lhs_variable] = rhs_value	
+	
+					else:
+						if current_token.type == 'NUMBER':
+							rhs_value = current_token.value
+						else:
+							rhs_value = variable_list[current_token.value]
+
+			if current_token.type == 'ASSIGN':
+				lhs_variable = previous_token.value
+				assign_flg = 1
+
+			previous_token = current_token
+			current_token = lexer.token()
+
+
+		next_edge = list(graph.edges(current_edge))
+		if len(next_edge) == 0:
+			break
+
+		i = 0
+		while i < len(next_edge):
+			statment_stack.append(next_edge[i][1])
+			i += 1
